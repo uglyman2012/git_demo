6f76af9a848ff59c52f857c8cb9b3d80583c24ac
ken <ken@qq.com>
2019-01-10 21:34:07 +0800

    不是不报

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -9,6 +9,6 @@ import java.util.ArrayList;
 public class Test1 {
     public static void main(String[] args) {
         ArrayList list=new ArrayList();
-
+        System.out.println("sakf");
     }
 }
