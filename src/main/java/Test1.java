a8464ed207b378e6526b5fd29007c97b655ba333
ken <ken@qq.com>
2019-01-10 21:14:03 +0800

    bug2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class Test1 {
     public static void main(String[] args) {
         ArrayList list=new ArrayList();
+        System.out.println("hadjfk");
 
     }
 }
